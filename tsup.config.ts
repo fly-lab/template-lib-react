fa920db928ddc38d680673061c3ee19c1c9de008
DHN Chandan <php.chandan@gmail.com>
2023-03-25 18:52:10 +0600

    chore: :wrench: Upgrade tsup config add index.tsx entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
-import type { Options } from 'tsup';
+import type { Options } from "tsup";
 
 const config: Options = {
-	entry: ["src/index.ts"],
+	entry: ["src/index.tsx"],
 	dts: true,
 	minify: "terser",
 	format: ["iife", "cjs", "esm"],
